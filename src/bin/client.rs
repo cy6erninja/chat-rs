7592671b55ba4438157eddf82a371f44352f51b1
cy6erninja <cy6erninja@gmail.com>
2024-04-02 01:50:54 +0200

    Ping! from client to server.

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,13 +1,20 @@
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     // 1. We need to connect to a server socket to be able to send messages there.
     let mut server_socket = TcpStream::connect("127.0.0.1:8000").unwrap();
-    // println!("{}", "Hello, I'm client");
-    //
+    println!("{}", "Hello, I'm client");
 
     println!("{:?}", server_socket);
 
-    server_socket.write(b"Hello, I'm your new client!").unwrap();
+    server_socket.write(b"Hello, I'm your new client!\n").unwrap();
+
+    loop {
+        server_socket.write(b"Ping!\n").unwrap();
+
+        sleep(Duration::new(1, 0));
+    }
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,6 +1,5 @@
-use std::io::{Read, Write};
-use std::net::{SocketAddr, TcpListener, TcpStream};
-use std::thread;
+use std::io::{ BufRead, BufReader };
+use std::net::{ SocketAddr, TcpListener, TcpStream };
 
 fn main() {
     println!("Hello, I'm Server!");
@@ -9,26 +8,47 @@ fn main() {
     //std::new::TcpListener is exactly that.
 
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    let mut last_client_id: usize = 0;
 
     loop {
         match listener.accept() {
-            Ok((socket, addr)) => handle_client(socket, addr),
+            Ok((socket, addr)) => handle_client(
+                socket,
+                addr,
+                {last_client_id +=1; last_client_id}
+            ),
             Err(e) => println!("couldn't get client: {e:?}")
         }
     }
-
-
 }
 
-fn handle_client(mut socket: TcpStream, addr: SocketAddr) {
+fn handle_client(mut socket: TcpStream, addr: SocketAddr, client_id: usize) {
 
     // 2. We need to have each new connection in a new thread.
     let mut res = String::new();
 
-    thread::spawn(move || {
+    // let mut res: [u8;10]= [0; 10];
+    //3. We need buffer from std::io to collect information from stream. This is because
+    // when data is sent over TCP, it may become fragmented and should be gathered in a buffer
+    // before using. Also, IO operations are more expensive than CPU operations, so reading
+    // as much as possible from a stream helps.
+
+
+    // thread::spawn(move || {
         // println!("new client: {addr:?}");
         // let _ = socket.write(b"Thanks for coming!");
-        socket.read_to_string(&mut res).unwrap();
-        println!("{res:?}");
-    });
+
+    let mut bufreader = BufReader::new(socket);
+
+    for line in bufreader.lines() {
+        match line {
+            Ok(l) => println!("Client {client_id:?}-> {l:?}"),
+            Err(_) => {}
+        };
+    }
+
+    // bufreader.read_line(&mut res).unwrap();
+    // println!("{client_id:?}-> {res:?}");
+
+    // });
 }
