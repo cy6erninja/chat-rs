ccc15725c113356d22ae02d7cf715d23ea0514f0
cy6erninja <cy6erninja@gmail.com>
2024-04-10 22:08:39 +0200

    Server sends received messages to other clients.

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,31 +1,35 @@
 use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 use std::net::TcpStream;
+use std::thread;
 use std::thread::sleep;
 use std::time::Duration;
 
 fn main() {
-    // 1. We need to connect to a server socket to be able to send messages there.
     let mut server_socket = TcpStream::connect("127.0.0.1:8000").unwrap();
-    // 2. To potentially improve performance by reducing IO operations, we need a write buffer.
-    let mut bufwriter = BufWriter::new(&server_socket);
-    let mut bufreader = BufReader::new(&server_socket);
+    let mut bufwriter = BufWriter::new(server_socket.try_clone().unwrap());
+    let mut bufreader = BufReader::new(server_socket);
 
     println!("{}", "Hello, I'm client");
 
     bufwriter.write(b"Hello, I'm your new client!\n").unwrap();
+    bufwriter.flush();
 
+    thread::spawn(move || {
+        loop {
+            bufwriter.write(b"Ping!\n").unwrap();
+            bufwriter.flush();
+
+            sleep(Duration::new(2, 0));
+        }
+    });
 
     loop {
         let mut line = String::new();
-        bufwriter.write(b"Ping!\n").unwrap();
-        bufwriter.flush();
 
         if bufreader.read_line(&mut line).unwrap() > 0 {
             let l = line.chars().filter(|c| *c != '\n').collect::<String>();
             println!("{l:?}");
             line.clear();
         }
-
-        sleep(Duration::new(1, 0));
     }
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,56 +1,77 @@
+use std::collections::HashMap;
+use std::hash::Hash;
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::net::{ SocketAddr, TcpListener, TcpStream };
+use std::sync::{Arc, mpsc, Mutex};
 use std::thread;
+
+struct Message {
+    pub from: SocketAddr,
+    pub message: String,
+}
 
 fn main() {
     println!("Hello, I'm Server!");
 
-    // 1. We need to listed for incoming connections from users.
-    //std::new::TcpListener is exactly that.
-
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
-    let mut last_client_id: usize = 0;
+    let stream_writers: HashMap<SocketAddr, BufWriter<TcpStream>> = HashMap::new();
+    let arc_writers = Arc::new(Mutex::new(stream_writers));
+    let broadcast_writers = Arc::clone(&arc_writers);
+
+    let (
+        sender,
+        receiver
+    ) = mpsc::channel::<Message>();
+
+    thread::spawn(move || {
+        loop {
+            let message = receiver.recv().unwrap();
+            let mut writers = broadcast_writers.lock().unwrap();
+
+            for (socket_addr, mut writer) in &mut *writers {
+                if *socket_addr == message.from {
+
+                    continue;
+                }
+
+                writer.write(message.message.as_bytes()).unwrap();
+                writer.write(b"\n").unwrap();
+                writer.flush().unwrap();
+            }
+        }
+    });
 
     loop {
         match listener.accept() {
-            Ok((socket, addr)) => handle_client(
-                socket,
-                addr,
-                {last_client_id +=1; last_client_id}
-            ),
+            Ok((socket, addr)) => {
+                arc_writers.lock().unwrap().insert(
+                    socket.peer_addr().unwrap(),
+                    BufWriter::new(socket.try_clone().unwrap())
+                );
+
+                let thread_sender = sender.clone();
+
+                thread::spawn(move || {
+                    let stream_reader = BufReader::new(&socket);
+                    let peer_addr = socket.peer_addr().unwrap();
+
+                    for line in stream_reader.lines() {
+                        match line {
+                            Ok(l) =>
+                                {
+                                    println!("Received message: {l:?}");
+                                    thread_sender.send(Message {
+                                        from: peer_addr,
+                                        message: l
+                                    }).unwrap();
+                                },
+                            Err(_) => {}
+                        };
+                    }
+                });
+
+            },
             Err(e) => println!("couldn't get client: {e:?}")
         }
     }
-}
-
-fn handle_client(mut socket: TcpStream, addr: SocketAddr, client_id: usize) {
-
-    // 2. We need to have each new connection in a new thread.
-    let mut res = String::new();
-
-    // let mut res: [u8;10]= [0; 10];
-    //3. We need buffer from std::io to collect information from stream. This is because
-    // when data is sent over TCP, it may become fragmented and should be gathered in a buffer
-    // before using. Also, IO operations are more expensive than CPU operations, so reading
-    // as much as possible from a stream helps.
-
-    thread::spawn(move || {
-
-        let mut bufwriter = BufWriter::new(&socket);
-        let mut bufreader = BufReader::new(&socket);
-
-        for line in bufreader.lines() {
-            match line {
-                Ok(l) =>
-                    {
-                        println!("Client {client_id:?}-> {l:?}");
-                        if l.eq("Ping!") {
-                            bufwriter.write(b"Pong!\n");
-                            bufwriter.flush();
-                        }
-                    },
-                Err(_) => {}
-            };
-        }
-    });
 }
